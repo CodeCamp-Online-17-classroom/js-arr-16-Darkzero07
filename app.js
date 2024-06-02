cbda47a0786b13093b3cc899fe08bda202f5f9b0
Darkzero7 <chaiyanituporn@gmail.com>
2024-06-02 19:20:56 +0700

    js lab 102

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-// write code here
+const dashedText = "background-color";
+
+let text = dashedText.split("-");
+// console.log(text);
+let text2 = text[1].charAt(0).toUpperCase() + text[1].slice(1);
+// console.log(text2);
+let textCombine = text[0].concat(text2);
+console.log(textCombine);
